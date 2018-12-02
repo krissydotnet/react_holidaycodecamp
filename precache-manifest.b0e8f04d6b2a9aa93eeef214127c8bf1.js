fb9c7e9205595ef41f3b02cf5cb0858330ae7da6
Krissy Dukes <krissydotnet@yahoo.com>
2018-12-02 12:35:28 -0500

    Updates

---
+++ precache-manifest.b0e8f04d6b2a9aa93eeef214127c8bf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "144c04051d4e737646eb",
+    "url": "/react_holidaycodecamp/static/js/runtime~main.144c0405.js"
+  },
+  {
+    "revision": "9eb73d56820b2625f8de",
+    "url": "/react_holidaycodecamp/static/js/main.9eb73d56.chunk.js"
+  },
+  {
+    "revision": "39a0acda35a094cf9c2c",
+    "url": "/react_holidaycodecamp/static/js/1.39a0acda.chunk.js"
+  },
+  {
+    "revision": "9eb73d56820b2625f8de",
+    "url": "/react_holidaycodecamp/static/css/main.4ad3ba31.chunk.css"
+  },
+  {
+    "revision": "c55f47851fcec0e50611135b5a88b9c4",
+    "url": "/react_holidaycodecamp/index.html"
+  }
+];
